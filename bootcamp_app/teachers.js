75ff4f7e464781ea314f1a408830dd88b2b3343e
JeremyWallace1 <jeremy.wallace@telus.com>
2022-12-14 16:45:52 -0700

    cleaning up code for values and using queryString to pass into pool.query statements for both students.js and teachers.js to keep things safe from hackers

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,22 +7,24 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const [ node, path, cohortName ] = process.argv;
-// console.log(process.argv);
-const queryVals = [`%${cohortName}%`];
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`];
 
-pool.query(`
-SELECT DISTINCT cohorts.name AS "cohort", teachers.name AS "teacher_name"
-FROM assistance_requests
-JOIN students ON assistance_requests.student_id = students.id
-JOIN teachers ON assistance_requests.teacher_id = teachers.id
-JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name LIKE $1
-ORDER BY teacher_name;
-`, queryVals)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.cohort}: ${user.teacher_name}`);
-  })
-}).catch(err => console.error('query error', err.stack))
-.finally(() => pool.end());
+const queryString = `
+  SELECT DISTINCT cohorts.name AS "cohort", teachers.name AS "teacher_name"
+  FROM assistance_requests
+  JOIN students ON assistance_requests.student_id = students.id
+  JOIN teachers ON assistance_requests.teacher_id = teachers.id
+  JOIN cohorts ON students.cohort_id = cohorts.id
+  WHERE cohorts.name LIKE $1
+  ORDER BY teacher_name;
+  `;
+
+pool.query(queryString, values)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.cohort}: ${user.teacher_name}`);
+    });
+  }).catch(err => console.error('query error', err.stack))
+  .finally(() => pool.end());

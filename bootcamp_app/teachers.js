bded36c3346b00cece96ccde9b9a1912ef58d564
JeremyWallace1 <jeremy.wallace@telus.com>
2022-12-14 15:50:39 -0700

    creating teachers.js and students.js with pool.query for teachers answering requests from a specific cohort and students in a specific cohort taken from process.argv arguments in node

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,20 +7,22 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const [ node, path, cohortName, numResults ] = process.argv;
+const [ node, path, cohortName ] = process.argv;
 // console.log(process.argv);
-const queryVals = [`%${cohortName}%`, numResults];
+const queryVals = [`%${cohortName}%`];
 
 pool.query(`
-SELECT students.id AS "student_id", students.name AS "name", cohorts.name AS "cohort"
-FROM students
+SELECT DISTINCT cohorts.name AS "cohort", teachers.name AS "teacher_name"
+FROM assistance_requests
+JOIN students ON assistance_requests.student_id = students.id
+JOIN teachers ON assistance_requests.teacher_id = teachers.id
 JOIN cohorts ON students.cohort_id = cohorts.id
 WHERE cohorts.name LIKE $1
-LIMIT $2;
+ORDER BY teacher_name;
 `, queryVals)
 .then(res => {
   res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    console.log(`${user.cohort}: ${user.teacher_name}`);
   })
 }).catch(err => console.error('query error', err.stack))
 .finally(() => pool.end());
